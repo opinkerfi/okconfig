7a5756e92d73fb27e1dcee632f75b3360d3e7048
Pall Sigurdsson <palli@opensource.is>
2013-10-07 14:32:46 +0000

    setup.py code cleanup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-## setup.py ###
+# setup.py ###
 from distutils.core import setup
 import os
 
@@ -9,6 +9,7 @@ LONG_DESC = """
 %s contains tools and templates for enterprise quality nagios configuration.
 """ % NAME
 
+
 def get_filelist(path):
     """Returns a list of all files in a given directory"""
     files = []
@@ -16,41 +17,41 @@ def get_filelist(path):
     while len(directories_to_check) > 0:
         current_directory = directories_to_check.pop(0)
         for i in os.listdir(current_directory):
-            if i == '.gitignore': continue
+            if i == '.gitignore':
+                continue
             relative_path = current_directory + "/" + i
-            if os.path.isfile(relative_path): files.append(relative_path)
-            elif os.path.isdir(relative_path): directories_to_check.append(relative_path)
-            else: print "what am i?", i
+            if os.path.isfile(relative_path):
+                files.append(relative_path)
+            elif os.path.isdir(relative_path):
+                directories_to_check.append(relative_path)
+            else:
+                print "what am i?", i
     return files
 
 if __name__ == "__main__":
-    manpath		= "share/man/man1/"
+    manpath = "share/man/man1/"
     etcpath = "/etc/%s" % NAME
-    etcmodpath	= "/etc/%s/modules" % NAME
-    initpath	= "/etc/init.d/"
-    logpath		= "/var/log/%s/" % NAME
-    varpath		= "/var/lib/%s/" % NAME
-    rotpath		= "/etc/logrotate.d"
-    datarootdir	= "/usr/share/%s" % NAME
+    etcmodpath = "/etc/%s/modules" % NAME
+    initpath = "/etc/init.d/"
+    logpath = "/var/log/%s/" % NAME
+    varpath = "/var/lib/%s/" % NAME
+    rotpath = "/etc/logrotate.d"
+    datarootdir = "/usr/share/%s" % NAME
     template_files = get_filelist('usr/share/okconfig')
-    data_files = map(lambda x: ("/" + os.path.dirname(x),[x]), template_files)
-    data_files.append( ( logpath, [] ) )
-    data_files.append( ( rotpath, [ "etc/logrotate.d/okconfig" ] ) )
+    data_files = map(lambda x: ("/" + os.path.dirname(x), [x]), template_files)
+    data_files.append((logpath, []))
+    data_files.append((rotpath, ["etc/logrotate.d/okconfig"]))
     #data_files.append(( manpath, ['docs/okconfig.1.gz'] ))
     setup(
         name='%s' % NAME,
-        version = VERSION,
+        version=VERSION,
         author='Pall Sigurdsson',
-        description = SHORT_DESC,
-        long_description = LONG_DESC,
+        description=SHORT_DESC,
+        long_description=LONG_DESC,
         author_email='palli@opensource.is',
         url='http://opensource.is/okconfig',
         license='GPL',
-        scripts = [
-            'usr/bin/okconfig',
-        ],
-        packages = [
-            'okconfig',
-        ],
-        data_files = data_files,
+        scripts=['usr/bin/okconfig'],
+        packages=['okconfig'],
+        data_files=data_files,
     )
