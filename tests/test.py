058dc5a21daedfe79235d4e12774617428d56d25
Tomas Edwardsson <tommi@tommi.org>
2014-05-30 00:01:50 +0000

    Added a few tests mostly to test addhost for now

---
+++ tests/test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import os
+import sys
+import unittest
+
+# Make sure we import from working tree
+okconfig_base = os.path.dirname(os.path.realpath(__file__ + "/.."))
+sys.path.insert(0, okconfig_base)
+
+import unittest
+import doctest
+
+from tests import tests_dir
+
+import okconfig
+
+def load_tests(loader=None, tests=None, pattern=None):
+    suite = unittest.TestSuite()
+
+    # Add doctesting
+    suite.addTest(doctest.DocTestSuite(okconfig))
+
+    # Load unit tests from all files starting with test_*
+    for all_test_suite in unittest.defaultTestLoader.discover(
+            '.',
+            pattern='test_*.py'):
+        for test_suite in all_test_suite:
+            suite.addTest(test_suite)
+    return suite
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
+
