f3e5dcfb55c06f50502ac1b574779bcfbd35b8cd
Tomas Edwardsson <tommi@tommi.org>
2014-06-06 23:13:29 +0000

    Tests, add okconfig templates and template tests

--- tests/test_add.py
+++ tests/test_add.py
@@ -3,7 +3,7 @@
 
 import os.path
 import sys
-
+from shutil import copytree
 
 # Make sure we import from working tree
 okconfig_base = os.path.dirname(os.path.realpath(__file__ + "/.."))
@@ -14,13 +14,15 @@ import okconfig
 from pynag.Utils.misc import FakeNagiosEnvironment
 from pynag import Model
 
-
 class Host(unittest.TestCase):
     """Tests pertaining to addhost"""
     def setUp(self):
         environment = FakeNagiosEnvironment()
         environment.create_minimal_environment()
+        copytree(os.path.realpath("../usr/share/okconfig/templates"),
+                   environment.tempdir + "/conf.d/okconfig-templates")
         environment.update_model()
+
         self.environment = environment
 
         self._okconfig_overridden_vars = {}
@@ -111,7 +113,104 @@ class Host(unittest.TestCase):
         self.assertTrue(len(hosts) == 1)
         self.assertEqual(hosts[0].alias, "The alias host")
 
+    def test_force(self):
+        """Test force adding a host"""
+        okconfig.addhost('www.okconfig.org')
 
+        self.assertRaises(okconfig.OKConfigError, okconfig.addhost,
+                          'www.okconfig.org')
+
+class Template(unittest.TestCase):
+    """Template additions tests"""
+    def setUp(self):
+        environment = FakeNagiosEnvironment()
+        environment.create_minimal_environment()
+        copytree(os.path.realpath("../usr/share/okconfig/templates"),
+                   environment.tempdir + "/conf.d/okconfig-templates")
+        environment.update_model()
+
+        self.environment = environment
+
+        self._okconfig_overridden_vars = {}
+        for var in ['nagios_config', 'destination_directory',
+                    'examples_directory', 'examples_directory_local']:
+            self._okconfig_overridden_vars[var] = getattr(okconfig, var)
+
+        okconfig.nagios_config = self.environment.get_config().cfg_file
+        okconfig.destination_directory = self.environment.objects_dir
+        okconfig.examples_directory = "../usr/share/okconfig/examples"
+        okconfig.examples_directory_local = environment.tempdir + "/okconfig"
+
+        os.mkdir(okconfig.examples_directory_local)
+
+        okconfig.addhost("www.okconfig.org")
+        okconfig.addhost("okconfig.org")
+        okconfig.addhost("aliased.okconfig.org",
+                         address="192.168.1.1",
+                         group_name="testgroup")
+
+
+    def tearDown(self):
+        #self.environment.terminate()
+        for var, value in self._okconfig_overridden_vars.items():
+            setattr(okconfig, var, value)
+
+    def test_basic(self):
+        """Add a template to a host"""
+        okconfig.addtemplate("www.okconfig.org", template_name="http")
+
+        services = Model.Service.objects.filter(
+            host_name="www.okconfig.org",
+            use="okc-check_http",
+            service_description="HTTP www.okconfig.org")
+
+        self.assertEquals(len(services), 1, "There can be only one")
+
+    def test_multiple(self):
+        """Add multiple templates to a host"""
+        okconfig.addtemplate('www.okconfig.org', template_name="http")
+        okconfig.addtemplate('www.okconfig.org', template_name="linux")
+
+        for use, description in [
+            ('okc-check_http', 'HTTP www.okconfig.org', ),
+            ('okc-linux-check_disks', 'Disk Usage', ),
+            ('okc-linux-check_load', 'Load', ),
+            ('okc-linux-check_swap', 'Swap Usage', ),
+        ]:
+            services = Model.Service.objects.filter(
+                host_name="www.okconfig.org",
+                use=use,
+                service_description=description)
+            self.assertEqual(1, len(services), "Unable to match %s %s" % (
+                use, description))
+
+    def test_conflict(self):
+        """Add conflicting template"""
+        okconfig.addtemplate('www.okconfig.org', template_name="http")
+
+        self.assertRaises(okconfig.OKConfigError,
+                          okconfig.addtemplate,
+                          host_name='www.okconfig.org',
+                          template_name='http')
+
+    def test_force(self):
+        """Test force adding a template"""
+        okconfig.addtemplate('www.okconfig.org', template_name="http")
+        okconfig.addtemplate('www.okconfig.org', template_name="http",
+                             force=True)
+
+    def test_group(self):
+        """Test adding template with group"""
+        okconfig.addtemplate("aliased.okconfig.org",
+                             "http",
+                             group_name="webgroup")
+
+        services = Model.Service.objects.filter(
+            host_name="aliased.okconfig.org",
+            service_description="HTTP aliased.okconfig.org",
+        )
+        self.assertEqual(1, len(services), "There can be only one")
+        self.assertEqual(services[0].contact_groups, "webgroup")
 
 if __name__ == "__main__":
     unittest.main()
