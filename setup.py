0a7a3e2a98c60174ab948b342291b282e3b2bf1f
Gardar Thorsteinsson <gardart@gmail.com>
2021-10-20 11:23:54 +0000

    Bumped version number to 1.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 import os
 
 NAME = "okconfig"
-VERSION = '1.4.1'
+VERSION = '1.4.2'
 SHORT_DESC = "%s - Powertools to generate Nagios configuration files" % NAME
 LONG_DESC = """
 %s contains tools and templates for enterprise quality nagios configuration.
